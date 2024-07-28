c0b66636b19324c2fe55b6fc64b9134a5ec8469d
Alexey <alexeishibakov@yandex.ru>
2024-07-28 11:54:02 +0300

    router

--- users/urls.py
+++ users/urls.py
@@ -1,14 +1,11 @@
-from django.urls import path
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework.routers import DefaultRouter
 
 from users.apps import UsersConfig
-from users.views import UserCreateView
+from users.views import UserViewSet
 
 app_name = UsersConfig.name
 
-urlpatterns = [
-    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+router = DefaultRouter()
+router.register(r'user', UserViewSet, basename='user')
 
-    path('create/', UserCreateView.as_view(), name='user_create'),
-]
+urlpatterns = [] + router.urls
--- users/views.py
+++ users/views.py
@@ -9,7 +9,7 @@ from users.serializers import AuthUserSerializer, UserSerializer
 
 # Create your views here.
 
-class UserCreateView(viewsets.ModelViewSet):
+class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
